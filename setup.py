18576facd1450e772a9f4c7f907da483645a0805
James Yu <jxy639@case.edu>
2021-03-07 03:58:33 -0500

    Update classifiers and fix to include hpp header files in distribution

--- setup.py
+++ setup.py
@@ -33,9 +33,18 @@ setup(
     url="https://github.com/JYProjs/patentpy",
     ext_modules=ext_modules,
     install_requires=["pandas", "numpy"],
-    classifiers=
-            "Programming Language :: Python :: 3"
-            "License :: OSI Approved :: MIT License"
+    classifiers= [
+            "Programming Language :: Python :: 3",
+            "Programming Language :: C++",
+            "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: Information Technology",
+            "Intended Audience :: Legal Industry",
+            "Natural Language :: English",
+            "Topic :: Utilities",
+            "Topic :: Text Processing :: General",
+            ],
     zip_safe=False,
+    python_requires='>=3',
 )
